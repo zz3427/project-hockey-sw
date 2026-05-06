2868d4d25d244b2bf36acbfa2b1252f0e26572c7
Gerald Zhao <zz3422@nyu.edu>
2026-05-06 17:54:29 -0400

    added scoring logic

--- app/game_config.h
+++ app/game_config.h
@@ -27,12 +27,6 @@
 #define PUCK_START_X   320.0
 #define PUCK_START_Y   240.0
 
-// Hardware rink geometry
-#define WALL_LEFT    10.0
-#define WALL_RIGHT   630.0
-#define WALL_TOP     10.0
-#define WALL_BOTTOM  470.0
-
 // Paddle movement bounds
 #define P1_X_MIN       (WALL_LEFT + PADDLE_RADIUS)
 #define P1_X_MAX       (320.0 - PADDLE_RADIUS)
